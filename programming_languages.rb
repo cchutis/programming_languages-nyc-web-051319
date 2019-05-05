301ffcb5d0840c47f878d08caca594daf498b8ea
Constantine Chutis <constantinec84@gmail.com>
2019-05-05 14:34:28 -0400

    Finished

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,13 @@
 def reformat_languages(languages)
   # your code here
+  new_hash = {}
+  languages.each do |style, lang|
+    lang.each do |name, att|
+      new_hash[name] = att
+      new_hash[name][:style] = []
+      new_hash[name][:style] << style
+    end
+  end
+  new_hash[:javascript][:style] << :oo
+  new_hash
 end
